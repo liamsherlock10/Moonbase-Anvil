09164efe621c7cfe8cfe6dd0fccc4e722178c589
liamsherlock55@gmail.com <liamsherlock55@gmail.com>
2025-03-17 10:26:45 +0000

    Edited Main.OutboundMail.ItemTemplate1 and Main.OutboundMail

--- client_code/Main/OutboundMail/ItemTemplate1/__init__.py
+++ client_code/Main/OutboundMail/ItemTemplate1/__init__.py
@@ -9,5 +9,5 @@ class ItemTemplate1(ItemTemplate1Template):
   def __init__(self, **properties):
     # Set Form properties and Data Bindings.
     self.init_components(**properties)
-
+    
     # Any code you write here will run before the form opens.
